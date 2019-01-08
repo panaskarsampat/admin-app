bc3cf78e9bda555bf0d054dc075fb6838830c731
Sampat Panaskar <33483552+panaskarsampat@users.noreply.github.com>
2019-01-08 16:55:58 +0530

    Item

---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pricing.Entities
+{
+    public class Item
+    {
+        public int ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public double IteamPrice { get; set; }
+        public int qty { get; set; }
+        public double GrandTotal { get; set; }
+    }
+}
